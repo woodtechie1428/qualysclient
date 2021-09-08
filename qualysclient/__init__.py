2a9cbed81f56264bf262a59528c5e35a1606db27
Wood Techie <woodtechie1428@gmail.com>
2021-09-08 17:04:07 -0400

    auth methods

--- qualysclient/__init__.py
+++ qualysclient/__init__.py
@@ -1,1 +1,34 @@
-__version__ = '0.0.1'
+
+from aiohttp import payload
+from qualysclient.defaults import AUTH_URI
+import requests
+
+
+class QualysClient(object):
+    
+    def __init__(self, username, password):
+
+        self.s = requests.Session()
+        self.s.headers.update({'X-Requested-With':'Qualys Client - Python'})
+
+        payload = {
+            'action': 'login',
+            'username': username,
+            'password': password
+        }
+
+        self.login(payload)
+
+    
+    def login(self, payload):
+        r = self.s.post(AUTH_URI, payload)
+        if (r.status_code != 200):
+            print('Status:', r.status_code, 'Headers:', r.headers, 'Error Response:', r.content)
+
+    def logout(self):
+        payload = {
+            'action': 'logout'
+        }
+        r = self.s.post(AUTH_URI, payload)
+        if (r.status_code != 200):
+            print('Status:', r.status_code, 'Headers:', r.headers, 'Error Response:', r.content)
