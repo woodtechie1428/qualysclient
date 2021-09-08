bd6c31d426fa1eae323b66942c97ae455e0b37cd
Wood Techie <woodtechie1428@gmail.com>
2021-09-08 16:19:18 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# qualysclient
+
+A python SDK for interacting with the Qualys API
+
+## Installation
+```
+python -m pip install qualysclient
+```
+
+## Example 
+
+```
+import qualysclient
+
+```
---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+exec(open('qualysclient/version.py').read())
+
+setup(
+    name="qualysclient",
+    version=__version__,
+    author="Wood Techie",
+    author_email="woodtechie1428@gmail.com",
+    description="Python SDK for interacting with the Qualys API",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/woodtechie1428/qualysclient",
+    project_urls={
+        "Bug Tracker": "https://github.com/woodtechie1428/qualysclient/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    python_requires=">=3.6",
+    py_modules=["qualysclient"],
+    install_requires=['requests']
+
+)
