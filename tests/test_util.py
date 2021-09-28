17b58a330412d338452c32a621f1665a96166775
Wood Techie <woodtechie1428@gmail.com>
2021-09-28 14:11:19 -0400

    added/modified tests

--- tests/test_util.py
+++ tests/test_util.py
@@ -1,6 +1,12 @@
 from requests.sessions import session
 from requests.exceptions import SSLError, Timeout
 from qualysclient import QualysClient, _util
+from qualysclient.exceptions import (
+    InvalidParameterError,
+    ParameterValidationError,
+    RequiredParameterMissingError,
+)
+import qualysclient
 from qualysclient.defaults import BASE_URI
 import pytest
 import requests
@@ -9,26 +15,39 @@ from unittest.mock import Mock
 
 
 def test_validate_parameters_api_action_as_None():
-    with pytest.raises(Exception) as e_info:
-        tr = _util._validate_parameters(None, ip=5)
-    # assert tr in [True, False]
+    with pytest.raises(qualysclient.exceptions.ParameterValidationError) as e_info:
+        tr = _util._validate_parameters(api_action=None, ip=5)
+    expected = "No api_action specified"
+    assert expected == str(e_info.value)
 
 
 def test_validate_parameters_api_action_valid():
-    tr = _util._validate_parameters("launch_report", id=5)
+    tr = _util._validate_parameters(api_action="launch_report", template_id=5)
     assert tr in [True, False]
 
 
 def test_validate_parameters_api_action_invalid():
-    with pytest.raises(Exception) as e_info:
-        tr = _util._validate_parameters("invalid_name", id=5)
+    with pytest.raises(ParameterValidationError) as e_info:
+        tr = _util._validate_parameters(api_action="invalid_name", id=5)
+    expected = "Invalid api_action specified"
+    assert expected == str(e_info.value)
 
 
-def test_validate_parameters_kwargs():
-    tr = _util._validate_parameters("list_ip", simple=5, st=7)
-    assert tr in [True, False]
+def test_validate_parameters_kwargs_valid():
     tr = _util._validate_parameters("launch_report", template_id=5)
     assert tr in [True, False]
+
+
+def test_validate_parameters_kwargs_invalid():
+    with pytest.raises((InvalidParameterError)) as e_info:
+        tr = _util._validate_parameters(api_action="list_ip", invalid_key1=5)
+
+
+def test_validate_parameters_missing_or_invalid_required_parameters():
+    with pytest.raises((RequiredParameterMissingError)) as e_info:
+        tr = _util._validate_parameters(
+            api_action="compliance_policy_import", xml_file="some_value"
+        )
 
 
 @responses.activate
@@ -95,17 +114,26 @@ def test_perform_request_URLRequired(qc):
         )
 
 
-def test_api_request_validation_fail(mocker):
+def test_perform_request_RequestException(qc):
+    qc.s.post.side_effect = requests.RequestException
+    with pytest.raises(requests.RequestException) as e_info:
+        tr = _util._perform_request(
+            qc, BASE_URI + "/api/2.0/fo/report/?action=list", None
+        )
+
+
+def test_api_request_validation_fail(mocker, qc):
     # mocker.patch('qualysclient._util._perform_request',
     #              return_value = True)
 
-    with pytest.raises(Exception) as e_info:
+    with pytest.raises(ParameterValidationError) as e_info:
         actual = _util._api_request(
-            QualysClient(),
-            BASE_URI + "/api/2.0/fo/report/?action=list",
-            input_params={"k": "v"},
+            caller=qc,
+            api_action="launch_report",
+            k="v",
         )
-    # assert expected == actual
+    # expected = "Parameter Validation failed"
+    # assert expected == str(e_info.value)
 
 
 def test_api_request_validation_pass(mocker):
