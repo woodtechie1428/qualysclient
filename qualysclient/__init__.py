74415af921d468e7fb78d4d87aab019dc98da055
woodtechie1428 <woodtechie1428@gmail.com>
2021-09-30 22:53:20 -0400

    move login functions to QualysClient

--- qualysclient/__init__.py
+++ qualysclient/__init__.py
@@ -3,6 +3,9 @@ from .reports import Reports
 from .compliance import Compliance
 from ._auth_session import AuthSession
 
+from requests import Session
+from qualysclient._defaults import AUTH_URI
+
 
 class QualysClient(AuthSession):
     """
@@ -16,6 +19,16 @@ class QualysClient(AuthSession):
             username=self.username, password=self.password
         )
 
+    def __init__(self, username=None, password=None):
+        self.username = username
+        self.password = password
+        self.s = Session()
+        self.s.headers.update({"X-Requested-With": "Qualys Client - Python"})
+
+        super(QualysClient, self).__init__(self.s)
+        if username:
+            self.login(username=username, password=password)
+
     def __enter__(self, username=None, password=None):
         return self
 
@@ -26,6 +39,49 @@ class QualysClient(AuthSession):
             traceback.print_exception(exc_type, exc_value, tb)
         self.logout()
         del self
+
+    def login(self, username, password):
+        """
+        Authenticate to Qualys API
+
+        :param username: Qualys Username
+        :type username: str
+        :param password: Qualys Password
+        :type password: str
+        """
+        print("login called")
+        if isinstance(username, str) is False or isinstance(password, str) is False:
+            raise Exception("username and password is required and must be of type str")
+
+        payload = {"action": "login", "username": username, "password": password}
+        r = self.s.post(AUTH_URI, payload)
+        if r.status_code != 200:
+            print(
+                "Status:",
+                r.status_code,
+                "Headers:",
+                r.headers,
+                "Error Response:",
+                r.content,
+            )
+
+        return self
+
+    def logout(self):
+        """
+        Log out of authenticated sessionn
+        """
+        payload = {"action": "logout"}
+        r = self.s.post(AUTH_URI, data=payload)
+        if r.status_code != 200:
+            print(
+                "\nStatus:",
+                r.status_code,
+                "\nHeaders:",
+                r.headers,
+                "\nError Response:",
+                r.content,
+            )
 
     @property
     def reports(self):
--- qualysclient/_auth_session.py
+++ qualysclient/_auth_session.py
@@ -1,5 +1,4 @@
 from requests import Session
-from qualysclient._defaults import AUTH_URI
 
 
 class AuthSession:
@@ -7,54 +6,5 @@ class AuthSession:
     Authenticates to Qualys API
     """
 
-    def __init__(self, username=None, password=None):
-        self.username = username
-        self.password = password
-        self.s = Session()
-        self.s.headers.update({"X-Requested-With": "Qualys Client - Python"})
-
-        if username:
-            self.login(username=username, password=password)
-
-    def login(self, username, password):
-        """
-        Authenticate to Qualys API
-
-        :param username: Qualys Username
-        :type username: str
-        :param password: Qualys Password
-        :type password: str
-        """
-        print("login called")
-        if isinstance(username, str) is False or isinstance(password, str) is False:
-            raise Exception("username and password is required and must be of type str")
-
-        payload = {"action": "login", "username": username, "password": password}
-        r = self.s.post(AUTH_URI, payload)
-        if r.status_code != 200:
-            print(
-                "Status:",
-                r.status_code,
-                "Headers:",
-                r.headers,
-                "Error Response:",
-                r.content,
-            )
-
-        return self
-
-    def logout(self):
-        """
-        Log out of authenticated sessionn
-        """
-        payload = {"action": "logout"}
-        r = self.s.post(AUTH_URI, data=payload)
-        if r.status_code != 200:
-            print(
-                "\nStatus:",
-                r.status_code,
-                "\nHeaders:",
-                r.headers,
-                "\nError Response:",
-                r.content,
-            )
+    def __init__(self, shared_session: Session):
+        self.s = shared_session
